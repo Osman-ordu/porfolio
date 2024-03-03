703b97ed394bb04e64047a0228bfbcfab93e2ebb
Osman Ordu <osman.ordu@primetech.com>
2024-03-03 18:04:14 +0300

    react-lazy-load was included

--- src/components/imageTextBox/index.jsx
+++ src/components/imageTextBox/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './styles.module.scss'; // Import styles
+import LazyLoad from 'react-lazy-load';
 
 const ImageTextBox = ({ image, title, subTitle, text }) => {
   return (
     <div className={styles['container']}>
       <div className={styles['container__widget__first']}>
-        <img src={image} alt={subTitle} />
+        <LazyLoad>
+          <img src={image} alt={subTitle} />
+        </LazyLoad>
       </div>
       <div className={styles['container__widget__second']}>
         <h2>{title}</h2>
