2b73e13e963010d88e376b3949b5141a6e6c0ee5
Osman.ordu <orduosmann@gmail.com>
2024-04-13 21:43:14 +0300

    Unused dependencies removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ToastContainer } from 'react-toastify';
 import { BrowserRouter as Router } from 'react-router-dom';
-import AppRoutes from './routes/index.jsx';
+import AppRoutes from './routes';
 import GeneralLayout from './components/layout/generalLayout/index.jsx';
-import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <GeneralLayout>
-      <ToastContainer />
       <Router>
         <AppRoutes />
       </Router>
     </GeneralLayout>
-  </React.StrictMode>
 );
