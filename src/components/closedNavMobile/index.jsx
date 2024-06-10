7ab000519e30ee9454b45f4c1c95d9f80e05ac16
Osman Ordu <osman.ordu@primetech.com>
2024-06-10 13:57:03 +0300

    Mobile navbar controller improvement

---
+++ src/components/closedNavMobile/index.jsx
@@ -0,0 +1,6 @@
+const closedNavMobile = () => {
+  const mobileNav = document.querySelector("#root > div > aside > div > ul");
+  mobileNav.style.display = "none";
+};
+
+export default closedNavMobile;
--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,20 +1,26 @@
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { Menu } from 'antd';
-import { menuData } from '../../mocks';
-import style from './styles.module.scss';
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { Menu } from "antd";
+import UseIsMobile from "../../components/isMobile";
+import closedNavMobile from "../../components/closedNavMobile";
+import { menuData } from "../../mocks";
+import style from "./styles.module.scss";
 
 const MenuGroup = ({ onMenuClick }) => {
   const { t } = useTranslation();
+  const isMobile = UseIsMobile();
 
-  const handleMenuClick = ({ key,icon }) => {
+  const handleMenuClick = ({ key, icon }) => {
     onMenuClick({ key, icon });
+    if (isMobile) {
+      closedNavMobile();
+    }
   };
 
   return (
-    <Menu className={style['c-sidemenu']} theme='dark' defaultSelectedKeys={['about']} mode='inline'>
+    <Menu className={style["c-sidemenu"]} theme="dark" defaultSelectedKeys={["about"]} mode="inline">
       {menuData.map((item) => (
-        <Menu.Item className={style['c-sidemenu__group']} key={t(item.key)} icon={item.icon} onClick={() => handleMenuClick(item)}>
+        <Menu.Item className={style["c-sidemenu__group"]} key={t(item.key)} icon={item.icon} onClick={() => handleMenuClick(item)}>
           <Link to={item.link}>{t(item.title)}</Link>
         </Menu.Item>
       ))}
